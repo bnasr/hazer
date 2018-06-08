19e44f6dc1ca013c7128757dc0dc83aab91ad5c9
bnasr <bijan.s.nasr@gmail.com>
2018-06-07 17:05:58 -0700

    test

--- tests/testthat/testall.R
+++ tests/testthat/testall.R
@@ -9,13 +9,21 @@ d <- getDarkness(img)
 c <- getContrast(img)
 h <- getHazeFactor(img)
 
+p <- plotRGBArray(img)
+
+
 test_that("test brightness, darkness and contrast", {
   expect_equal(floor(mean(b)*1000), 606)
   expect_equal(floor(mean(d)*1000), 364)
   expect_equal(floor(mean(c)*1000), 242)
 })
 
-test_that("test  is number of characters", {
+test_that("test the haze", {
   expect_equal(floor(h$haze*1000), 224)
   expect_equal(floor(h$A0*1000), 712)
 })
+
+test_that("test the plot", {
+  expect_equal(is.null(p), TRUE)
+})
+
